a033296c3482656a827d2ac8bfd5f4ea7e74fda0
dsoliyev55-arch <dsoliyev55@gmail.com>
2026-08-08 20:57:17 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Salom! Bot Render'da muvaffaqiyatli ishlayapti! 🚀")
+
+if __name__ == "__main__":
+    print("Bot ishga tushdi...")
+    bot.infinity_polling()
 
